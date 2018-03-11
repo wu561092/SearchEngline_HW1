64bfe407ca1a633e1ffc3826cdf5fce9bec0cd68
wu561092 <wu561092@gmail.com>
2018-03-12 00:48:40 +0800

    ver2

--- app.py
+++ app.py
@@ -29,32 +29,35 @@ def info():
 def result():
    if request.method == 'POST':
       result = request.form['search']
-      res = es.search(index='test_index', body={  'query': {    'match': {      'title': result,     }  }})
-      tmplist=[]
+      res = es.search(index='news', body={  'query': {    'match': {      'title': result,     }  }})
+      all_body_list=[]
+      all_title_list=[]
+      all_url_list=[]
       for doc in res['hits']['hits']:
-          print(" %s" % ( doc['_source']['author']))
-          tmplist.append(doc['_source']['author'])
-      return render_template("result.html",result=tmplist)
+          all_body_list.append(doc['_source']['body'])
+          all_title_list.append(doc['_source']['title'])
+          all_url_list.append(doc['_source']['url'])
+      return render_template("result.html",all_body=all_body_list,all_title=all_title_list,all_url=all_url_list)
 
 
 if __name__ == '__main__':
-    es.index(index='test_index', doc_type='post', id=1, body={
-        'author': 'John Doe',
-        'blog': 'Learning Elasticsearch',
-        'title': 'Using Python with Elasticsearch',
-        'tags': ['python', 'elasticsearch', 'tips'],
+    es.index(index='news', doc_type='post', id=1, body={
+        'title': '外星人回到地球了',
+        'url': 'http://travel.ettoday.net/article/1.htm',
+        'body':'記者吳光中／台北報導 熄燈3年半，ETtoday回來了，這次不叫「達康」，改叫「達內」，如果把ETtoday.net的net解釋為new ET，倒也還蠻有趣的',
     })
-    es.index(index='test_index', doc_type='post', id=2, body={
-        'author': 'John Doe2',
-        'blog': 'Learning Elasticsearch',
-        'title': 'Using Python with Elasticsearch is not happy',
-        'tags': ['python', 'elasticsearch', 'tips'],
+
+
+    es.index(index='news', doc_type='post', id=2, body={
+        'title': '2011年第46屆電視金鐘奨完整入圍名單',
+        'url': 'http://travel.ettoday.net/article/3.htm',
+        'body':'熄燈3年半，ETtoday回來了，這次不叫「達康」，改叫「達內」，如果把ETtoday.net的net解釋為new ET，倒也還蠻有趣的；既然叫new ET，總要有點創新吧？',
     })
-    es.index(index='test_index', doc_type='post', id=3, body={
-        'author': 'John Doe3',
-        'blog': 'Learning Elasticsearch',
-        'title': 'Using Python with Elasticsearch is maybe not happy',
-        'tags': ['python', 'elasticsearch', 'tips'],
+
+    es.index(index='news', doc_type='post', id=3, body={
+        'title': '外星人回到地球了22222',
+        'url': 'http://travel.ettoday.net/article/1.htm2222',
+        'body': '222記者吳光中／台北報導 熄燈3年半，ETtoday回來了，這次不叫「達康」，改叫「達內」，如果把ETtoday.net的net解釋為new ET，倒也還蠻有趣的',
     })
 
     app.run()
